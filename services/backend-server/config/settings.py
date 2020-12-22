8fe81beb4ad26215cfb32fdce75e4f3eaf12f70d
cyberdroidmann <cyberlexix@gmail.com>
2020-12-22 13:52:03 +0100

    added Docker.prod file and turn off browsable API in pro

--- services/backend-server/config/settings.py
+++ services/backend-server/config/settings.py
@@ -116,3 +116,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+
+if not DEBUG:
+    REST_FRAMEWORK = {
+        "DEFAULT_RENDERER_CLASSES": (
+            "rest_framework.renderers.JSONRenderer",
+        )
+    }
