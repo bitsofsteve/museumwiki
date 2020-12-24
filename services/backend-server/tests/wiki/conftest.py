8b5977c8146dd0cf718b5a34a2308ae9403a08d2
cyberdroidmann <cyberlexix@gmail.com>
2020-12-24 20:54:00 +0100

    fix conftest

--- services/backend-server/tests/wiki/conftest.py
+++ services/backend-server/tests/wiki/conftest.py
@@ -17,3 +17,17 @@ def add_wiki():
         return wiki
 
     return _add_wiki
+
+
+@pytest.fixture(autouse=True)
+def whitenoise_autorefresh(settings):
+    """
+    Get rid of whitenoise "No directory at" warning, as it's not helpful when running tests.
+
+    Related:
+        - https://github.com/evansd/whitenoise/issues/215
+        - https://github.com/evansd/whitenoise/issues/191
+        - https://github.com/evansd/whitenoise/commit/4204494d44213f7a51229de8bc224cf6d84c01eb
+    """
+    settings.WHITENOISE_AUTOREFRESH = True
+    
